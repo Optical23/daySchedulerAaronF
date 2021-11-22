791e01426cfaed0eee242c95d225c50271a0ee3c
Optical23 <40125489+Optical23@users.noreply.github.com>
2021-11-21 21:11:55 -0600

    added comments and a Readme with screenshots

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Work Day Scheduler Starter Code
+# Work Day Scheduler Aaron Ferguson
+The way I completed this challenge was by first writing out what I needed in html and adding the bootstrap and knowing what I wanted before trying to build it dynamically
+
+Once I got to the Script.js file I started with the simple display time which I just used the moment api for
+![Screenshot](./assets/images/screenshot3.PNG)
+
+Then I already knew the html I needed to dynamically generate my rows for the day scheduler and had a for loop iterate them
+![Screenshot](./assets/images/screenshot1.PNG)
+
+I call a few extra functions in the screen shots above to change military time and the index number to am or pm. Also to decide the background color of the input boxes to show if it is the current hour, past hour or future hour. As well as save to local storage so even when refreshed the tasks persist only if they were saved with the save/submit button.
+![Screenshot](./assets/images/screenshot2.PNG)
--- assets/js/script.js
+++ assets/js/script.js
@@ -6,8 +6,9 @@ currentDayheader.textContent = moment().format("dddd, MMMM Do");
 headerJumbotron.appendChild(currentDayheader);
 
 
-//Tasks time sheet
+//Making day scheduler
 var mainContainer = document.querySelector("main");
+//Started index with 9 so it would match the hour that was being built
 for (let i = 9; i < 18; i++) {
     buildSchedule(i);
 }
@@ -15,12 +16,12 @@ for (let i = 9; i < 18; i++) {
 function buildSchedule(index){
     //local storage check for saved tasks
     var savedtasks = localStorage.getItem(timeset(index)+"tasks");
-
+    //Creating hour rows container
     var rowContainer = document.createElement("div");
     rowContainer.setAttribute("class", "row w-100 m-auto px-3");
     rowContainer.setAttribute("style", "height:80px; padding-top:3px;")
     
-      
+    //Creating the sections of the row. It goes row[time column, input column, submit button]
     var timeCol = document.createElement("div");
     timeCol.setAttribute("class", "col-1 p-2 px-3 text-right border border-left-0");
     var inputCol = document.createElement("div");
@@ -30,16 +31,17 @@ function buildSchedule(index){
     submitbutton.setAttribute("type", "submit");
     submitbutton.innerHTML = "<i class='far fa-save'></i>";
     submitbutton.setAttribute("data-id", index);
-
+    //Creating the time on the time column
     var time = document.createElement("p");
     time.setAttribute("class", "col p-0");
     time.textContent = timeset(index);
+    //Creating the input element for the input column
     var todoinput = document.createElement("input");
     todoinput.setAttribute("class", inputclass(index));
     todoinput.setAttribute("id", index);
     todoinput.value = savedtasks;
     
-    
+    //Appending all the elements of the row
     mainContainer.appendChild(rowContainer);
     rowContainer.appendChild(timeCol);
     rowContainer.appendChild(inputCol);
@@ -50,7 +52,7 @@ function buildSchedule(index){
 
 
 }
-
+//Called to turn military time to am or pm
 function timeset(index){
     returnString = "";
     if(index < 13){
@@ -61,7 +63,7 @@ function timeset(index){
         return returnString;
     }
 }
-
+//Called to controll background color of the rows by checking against the time
 function inputclass(index){
     realTime = moment().hour();
     if(index < realTime){
@@ -72,7 +74,7 @@ function inputclass(index){
         return "future w-100 h-100 p-0";
     } 
 }
-console.log(realTime);
+
 //saving to local storage when save button is clicked
 mainContainer.addEventListener("click", function(event){
     var element = event.target;
